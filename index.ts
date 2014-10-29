45ee187b7e75a254791992da3907613d3cec7c10
Cellule <mike.ferris@hotmail.com>
2014-10-29 15:23:01 -0400

    Rename utils.MetaData to utils.MetaDataBuilder. Bonus exporting getLogger in utils

--- index.ts
+++ index.ts
@@ -1,17 +1,30 @@
 /// <reference path="typings/tsd.d.ts" />
-export import MetaData = require("./lib/metaData");
-export import ModuleControllersBinder = require("./lib/ModuleControllersBinder");
-export import BaseModule = require("./lib/BaseModule");
-
 var env = process.env;
 var TEST_ENV = env.TEST_ENV;
 var NODE_ENV = env.NODE_ENV || "development";
 
 // In development.
 export var __DEV__ = (NODE_ENV === "development");
-
 // In production.
 export var __PROD__ = (NODE_ENV === "production");
+
+export import getLogger = require("mykoop-logger");
+var logger = getLogger(module);
+
+export import MetaDataBuilder = require("./lib/MetaDataBuilder");
+export class MetaData extends MetaDataBuilder {
+  constructor() {
+    logger.warn(
+      "MetaData is deprecated, use MetaDataBuilder instead",
+      {
+        stack:new Error().stack
+      }
+    );
+    super();
+  }
+};
+export import ModuleControllersBinder = require("./lib/ModuleControllersBinder");
+export import BaseModule = require("./lib/BaseModule");
 
 // FIXME:: Test cases, pending real test infrastructure
 // this imports the file to trigger typescript compilation

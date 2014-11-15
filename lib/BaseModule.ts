f3e4075749810bb27271bbf942f5ed23e9371b7a
Cellule <mike.ferris@hotmail.com>
2014-11-14 20:56:17 -0500

    Added a method to all modules to call a version of a method that requires a connection by handling the connection for you.

--- lib/BaseModule.ts
+++ lib/BaseModule.ts
@@ -1,3 +1,5 @@
+import DatabaseError = require("../errors/DatabaseError");
+
 class BaseModule implements mykoop.IModule {
   moduleManager: mykoop.ModuleManager;
 
@@ -6,6 +8,26 @@ class BaseModule implements mykoop.IModule {
   }
   setModuleManager(moduleManager: mykoop.ModuleManager): void {
     this.moduleManager = moduleManager;
+  }
+
+  private callWithConnection(
+    method: (connection, params, callback) => void,
+    params: any,
+    callback: (err, result?) => void
+  ) {
+    var self: any = this;
+    if(typeof method === "string") {
+      method = self["__" + method];
+    }
+    self.db.getConnection(function(err, connection, cleanup) {
+      if(err) {
+        return callback(new DatabaseError(err));
+      }
+      method(connection, params, function(err, result) {
+        cleanup();
+        callback(err, result);
+      });
+    });
   }
 }
 export = BaseModule;

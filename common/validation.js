502f785f1a53718db3c04ad3fa828bab9106890b
Cellule <mike.ferris@hotmail.com>
2014-12-01 19:50:22 -0500

    Fixed validators for dates

--- common/validation.js
+++ common/validation.js
@@ -1,13 +1,35 @@
 // http://validatejs.org/ for documentation
 var validateJs = require("validate.js");
+var _ = require("lodash");
 validateJs.validators.datetime.parse = function (value) {
     if (value) {
+        if (_.isDate(value)) {
+            return value.getTime();
+        }
         return new Date(value).getTime();
     }
     return NaN;
 };
 validateJs.validators.datetime.format = function (value) {
     return new Date(value).toString();
+};
+validateJs.validators.presence = function (value, options) {
+    var message = options.message || "can't be blank";
+    if (_.isFunction(value)) {
+        return;
+    }
+    if (_.isDate(value)) {
+        return;
+    }
+    if (_.isString(value)) {
+        if ((/^\s*$/).test(value)) {
+            return message;
+        }
+        return;
+    }
+    if (_.isEmpty(value)) {
+        return message;
+    }
 };
 function validation(obj, constraint, options) {
     return validateJs(obj, constraint, options);
